94b753851c3b318dbb9267ff7b4a38772ab45684
Corei913th <bogningfredy972@gmail.com>
2026-04-24 14:06:50 +0100

    fix(cors): resolve production CORS issues and update theme to blue

--- formatrack_api/config/cors.php
+++ formatrack_api/config/cors.php
@@ -19,14 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL'),
-        'https://formatrack.cdwfs.net',
-        'http://localhost:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:3000',
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -36,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
